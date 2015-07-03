772e4b8c99c01fafaf994c83f89da937e2806cf2
qianlv <qianlv7@qq.com>
2015-07-03 08:20:11 -0700

    添加cop

--- the_practice_of_programming/list.c
+++ the_practice_of_programming/list.c
@@ -117,10 +117,68 @@ Nameval *delitem(Nameval *listp, char *name)
             else
                 prev->next = p->next;
             free(p);
-            return lisp;
+            return listp;
         }
         prev = p;
     }
     eprintf("delitem: %s not in list", name);
     return NULL;
+}
+
+Nameval *copy(Nameval *old_list)
+{
+    Nameval *new_list, *p;
+    new_list = (Nameval *)emalloc(sizeof(Nameval));
+    p = new_list;
+    for (; old_list != NULL; old_list = old_list->next)
+    { 
+        p->next = newitem(old_list->name, old_list->value);
+        p = p->next;
+    }
+    p->next = NULL;
+    p = new_list->next;
+    free(new_list);
+    return p;
+}
+
+void print(Nameval *listp)
+{
+    Nameval *p;
+    printf("{ ");
+    for (p = listp; p != NULL; p = p->next)
+    {
+        if (p != listp)
+            printf(",");
+        printf("%s:%d", p->name, p->value);
+    }
+    printf(" }\n");
+}
+
+Nameval *create_list_for_test(int n)
+{
+    Nameval *head = NULL;
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        char name[20];
+        sprintf(name, "key_%d", i);
+        head = addfront(head, newitem(name, i));
+    }
+    return head;
+    
+}
+void test_copy()
+{
+    Nameval *head, *new;
+    head = create_list_for_test(0);
+    head = create_list_for_test(1);
+    head = create_list_for_test(2);
+    print(head);
+    new = copy(head);
+    print(new);
+}
+
+int main()
+{ 
+    test_copy();
 }
