82e92bfba56af8c801fe2bfa6f2268a3eb8a4f74
qianlv <qianlv7@qq.com>
2015-08-12 19:38:09 +0800

    web server

---
+++ python/web_server/client.py
@@ -0,0 +1,47 @@
+# encoding=utf-8
+
+import argparse
+# import errno
+import os
+import socket
+
+
+SERVER_ADDRESS = 'localhost', 8888
+REQUEST = b"""\
+GET /hello HTTP/1.1
+Host: localhost:8888
+"""
+
+
+def main(max_clients, max_conns):
+    socks = []
+    for client_num in range(max_clients):
+        pid = os.fork()
+        if pid == 0:
+            for connection_num in range(max_conns):
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                sock.connect(SERVER_ADDRESS)
+                sock.sendall(REQUEST)
+                socks.append(sock)
+                print (connection_num)
+                os._exit(0)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Test client for simple web server',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        '--max-conns',
+        type=int,
+        default=1024,
+        help='Maximum number of connetions per client.'
+    )
+    parser.add_argument(
+        '--max-clients',
+        type=int,
+        default=1,
+        help='Maximum number of clients.'
+    )
+    args = parser.parse_args()
+    main(args.max_clients, args.max_conns)
---
+++ python/web_server/webserver3d.py
@@ -0,0 +1,45 @@
+# encoding=utf-8
+
+import socket
+import os
+
+SERVER_ADDRESS = (HOST, PORT) = '', 8888
+REQUEST_QUEUE_SIZE = 5
+
+
+def handle_request(client_connection):
+    request = client_connection.recv(1024)
+    print (request.decode())
+    http_response = b"""\
+HTTP/1.1 200 OK
+
+Hello, World!
+"""
+    client_connection.sendall(http_response)
+
+
+def serve_forever():
+    listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    listen_socket.bind(SERVER_ADDRESS)
+    listen_socket.listen(REQUEST_QUEUE_SIZE)
+    print ('serving HTTP on port {port} ...'.format(port=PORT))
+
+    clients = []
+    while True:
+        client_connection, client_address = listen_socket.accept()
+        # store the reference otherwise it's garbage collected
+        # on the next loop run
+        clients.append(client_connection)
+        pid = os.fork()
+        if pid == 0:                    # child process
+            listen_socket.close()
+            handle_request(client_connection)
+            client_connection.close()
+            os._exit(0)
+        else:                           # parent
+            # client_connection.close()
+            print(len(clients))
+
+if __name__ == '__main__':
+    serve_forever()
