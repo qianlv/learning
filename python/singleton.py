44e5ac4df9a51f7496e4f3eaed85532a63b38041
qianlv <qianlv7@qq.com>
2015-09-12 19:45:32 +0800

    singleton

---
+++ python/singleton.py
@@ -0,0 +1,20 @@
+# encoding=utf-8
+
+class Singleton(object):
+    @staticmethod
+    def instance():
+        if not hasattr(Singleton, '_instance'):
+            Singleton._instance = Singleton()
+        return Singleton._instance
+
+    def input(self, a):
+        print a
+
+if __name__ == '__main__':
+    t = Singleton.instance()
+    tt = Singleton.instance()
+    ttt = Singleton.instance()
+    t.input('abv')
+
+
+
