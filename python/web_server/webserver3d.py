6b72f2fa36e44ee063c7981c3630745d28408e05
qianlv <qianlv7@qq.com>
2015-09-12 23:59:06 +0800

    格式

--- python/web_server/webserver3d.py
+++ python/web_server/webserver3d.py
@@ -9,7 +9,7 @@ REQUEST_QUEUE_SIZE = 5
 
 def handle_request(client_connection):
     request = client_connection.recv(1024)
-    print (request.decode())
+    print request.decode()
     http_response = b"""\
 HTTP/1.1 200 OK
 
@@ -23,11 +23,11 @@ def serve_forever():
     listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     listen_socket.bind(SERVER_ADDRESS)
     listen_socket.listen(REQUEST_QUEUE_SIZE)
-    print ('serving HTTP on port {port} ...'.format(port=PORT))
+    print 'serving HTTP on port {port} ...'.format(port=PORT)
 
     clients = []
     while True:
-        client_connection, client_address = listen_socket.accept()
+        client_connection, _ = listen_socket.accept()
         # store the reference otherwise it's garbage collected
         # on the next loop run
         clients.append(client_connection)
@@ -39,7 +39,7 @@ def serve_forever():
             os._exit(0)
         else:                           # parent
             # client_connection.close()
-            print(len(clients))
+            print len(clients)
 
 if __name__ == '__main__':
     serve_forever()
